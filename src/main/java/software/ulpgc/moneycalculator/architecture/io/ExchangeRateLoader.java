7f757d288078e58fa3cf3c6600ce85cc045a8742
kiliansd21 <kiliansantanadelgado@gmail.com>
2026-01-14 12:05:48 +0000

    implement Money domain models and Loader interfaces

---
+++ src/main/java/software/ulpgc/moneycalculator/architecture/io/ExchangeRateLoader.java
@@ -0,0 +1,8 @@
+package software.ulpgc.moneycalculator.architecture.io;
+
+import software.ulpgc.moneycalculator.architecture.model.Currency;
+import software.ulpgc.moneycalculator.architecture.model.ExchangeRate;
+
+public interface ExchangeRateLoader {
+    ExchangeRate load(Currency from, Currency to);
+}
