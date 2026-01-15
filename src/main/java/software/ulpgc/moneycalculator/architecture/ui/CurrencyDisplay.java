d6adf8fd3ca33c85dec02224a0b6744bd25acad3
kiliansd21 <kiliansantanadelgado@gmail.com>
2026-01-15 19:03:29 +0000

    add CurrencyDisplay interface

---
+++ src/main/java/software/ulpgc/moneycalculator/architecture/ui/CurrencyDisplay.java
@@ -0,0 +1,7 @@
+package software.ulpgc.moneycalculator.architecture.ui;
+
+import software.ulpgc.moneycalculator.architecture.model.Currency;
+
+public interface CurrencyDisplay {
+    void show(Currency currency);
+}
